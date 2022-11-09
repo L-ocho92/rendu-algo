4347392e7e37874c956c396aad21498509e542ab
Victor <117074678+L-ocho92@users.noreply.github.com>
2022-11-09 16:53:16 +0100

    Add files via upload

---
+++ exercice/main.js
@@ -0,0 +1,87 @@
+// Exercice 1
+// let person = {
+//     name: 'John',
+//     santemental: 10,
+// }
+
+// const musics = [
+//     "Wejdene - Anissa",
+//     "Werenoi - Ordinateur",
+//     "Jul - Wsh Alors",
+//     "SCH - A7",
+//     "Gazo - Euphon",
+// ]
+
+// let trajet = {
+//     changes: 0,
+//     radio: musics[0],
+//     redlights: 30,
+
+
+// }
+// Math.floor
+
+// function taketaki(person, trajet) {
+//     while (trajet.redlights > 0) {
+//         if (musics[0]) {
+//             person.santemental--,
+//                 trajet.changes++,
+//                 console.log('Si Annisa passe a la radio le bon vieux john va perdre 1 hp et va changer de taxi ' + person.santemental + 'il prend donc ' + trajet.changes + 'le trajet continue et il reste ' + trajet.redlights + 'feux rouges')
+//         } else {
+//             console.log(musics + 'ecoute la radio')
+//         }
+
+//         trajet.redlights--
+//         if (trajet.redlights ==0 ) {
+//             console.log('John est vivant')
+//             break
+//         } else if (person.sentemental ==0) {
+//             console.log('John est mort')
+//             break
+//         }
+//     }
+
+// }
+// taketaki(person, trajet)
+
+// Exercice 1
+
+
+
+let killer = {
+    nom : 'Hector',
+    vie : 100,
+}
+
+class survivants {
+    connstructor(nom, attack, defense, vie, chance) {
+    this.names
+    this.caracs
+    }
+}
+
+const caracs = [ 'Sportif','Rapide','Fiotte','Drole','somnanbule']
+
+const names = ['Victor','Mc','Chad','Quentin','Oumar',]
+
+const fate = ['mort', 'esquiver', 'dommageavantlamort']
+
+function aléatoirenames(){
+    return Math.floor(Math.raléatoire()*5)
+}
+function aléatoirecarcs(){
+    return Math.floor(Math.aléatoire()*5)
+}
+function aléatoirefate(){
+    return Math.floor(Math.aléatoire()*3)
+}
+
+let fight1 = new survivants(noms[aléatoirenames], caracs[aléatoirecarcs], fate[aléatoirefate])
+let fight2 = new survivants(noms[aléatoirenames], caracs[aléatoirecarcs], fate[aléatoirefate])
+let fight3 = new survivants(noms[aléatoirenames], caracs[aléatoirecarcs], fate[aléatoirefate])
+let fight4 = new survivants(noms[aléatoirenames], caracs[aléatoirecarcs], fate[aléatoirefate])
+let fight5 = new survivants(noms[aléatoirenames], caracs[aléatoirecarcs], fate[aléatoirefate])
+let fighters = (fight1, fight2, fight3, fight4, fight5)
+
+console.log(fight1)
+
